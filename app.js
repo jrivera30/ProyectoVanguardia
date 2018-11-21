5d85334ce209609c113583301aa62b023f862aa8
Jesus Rivera <jesusalberto_rl@unitec.edu>
2018-11-21 16:41:42 -0600

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/indexUsuario');
-var usersRouter = require('./routes/users');
+//var usersRouter = require('./routes/users');
 
 var app = express();
 
